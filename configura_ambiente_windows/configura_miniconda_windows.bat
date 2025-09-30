be2b932ed02ee25f5ece2bb99f63a1e11757c484
Roberto Neto <164418185+robertopfneto@users.noreply.github.com>
2025-09-30 10:56:49 -0400

    Update configura_miniconda_windows.bat

--- configura_ambiente_windows/configura_miniconda_windows.bat
+++ configura_ambiente_windows/configura_miniconda_windows.bat
@@ -1,24 +1,4 @@
-@echo off
-echo ==========================================
-echo Instalando miniconda
-echo ==========================================
-
-REM Criar ambiente virtual com venv nativo
-python -m venv bot_telegram
-
-REM Ativar ambiente
-call bot_telegram\Scripts\activate.bat
-
-REM Atualizar pip
-python -m pip install --upgrade pip
-
-REM Instalar dependencias
-pip install -r requirements.txt
-
-echo ==========================================
-echo Ambiente configurado com sucesso!
-echo Agora execute:
-echo     bot_telegram\Scripts\activate
-echo     python bot_telegram.py SEU_TOKEN_AQUI
-echo ==========================================
-pause
+echo "Instalando miniconda"
+call curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o .\miniconda.exe
+call start /wait "" .\miniconda.exe /S
+call del .\miniconda.exe
